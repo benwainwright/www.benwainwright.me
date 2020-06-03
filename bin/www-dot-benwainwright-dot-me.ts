1e88457b7aabc8707e8ccad0dce62523cef39137
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-03 23:07:06 +0100

    Initial commit

--- bin/www-dot-benwainwright-dot-me.ts
+++ bin/www-dot-benwainwright-dot-me.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { WwwDotBenwainwrightDotMeStack } from '../lib/www-dot-benwainwright-dot-me-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { WwwDotBenwainwrightDotMeStack } from "../lib/www-dot-benwainwright-dot-me-stack";
 
 const app = new cdk.App();
-new WwwDotBenwainwrightDotMeStack(app, 'WwwDotBenwainwrightDotMeStack');
+new WwwDotBenwainwrightDotMeStack(app, "WwwDotBenwainwrightDotMeStack", {
+  env: {
+    region: "us-west-2",
+    account: "661272765443",
+  },
+});
--- lib/www-dot-benwainwright-dot-me-stack.ts
+++ lib/www-dot-benwainwright-dot-me-stack.ts
@@ -1,9 +1,32 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as alias from "@aws-cdk/aws-route53-targets";
+
+import { BlockPublicAccess } from "@aws-cdk/aws-s3";
 
 export class WwwDotBenwainwrightDotMeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const [recordName, domainName] = ["www", "benwainwright.me"];
+
+    const bucket = new s3.Bucket(this, "BensStaticWebsiteBucket", {
+      bucketName: `${recordName}.${domainName}`,
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+    });
+
+    const zone = new route53.HostedZone(this, "BensWebsiteBucket", {
+      zoneName: `${recordName}.${domainName}`,
+    });
+
+    new route53.ARecord(this, "BensWebsiteBucketRecord", {
+      zone,
+      recordName,
+      target: route53.RecordTarget.fromAlias(
+        new alias.BucketWebsiteTarget(bucket)
+      ),
+    });
   }
 }
