36d09c4f4dfc411d1ffe18e38f6ad30ad535902d
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-26 11:51:46 +0100

    Add deployment

--- lib/www-dot-benwainwright-dot-me-stack.ts
+++ lib/www-dot-benwainwright-dot-me-stack.ts
@@ -3,8 +3,7 @@ import * as s3 from "@aws-cdk/aws-s3";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as route53Targets from "@aws-cdk/aws-route53-targets";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
-
-import { BlockPublicAccess } from "@aws-cdk/aws-s3";
+import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
 
 export class WwwDotBenwainwrightDotMeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -15,7 +14,12 @@ export class WwwDotBenwainwrightDotMeStack extends cdk.Stack {
     const bucket = new s3.Bucket(this, "BensStaticWebsiteBucket", {
       bucketName: `${recordName}.${domainName}`,
       publicReadAccess: true,
-      websiteIndexDocument: "index.html",
+      websiteIndexDocument: "index.html"
+    });
+
+    new s3Deploy.BucketDeployment(this, "BensWebsiteDeployment", {
+      sources: [s3Deploy.Source.asset("../www")],
+      destinationBucket: bucket
     });
 
     const distribution = new cloudfront.CloudFrontWebDistribution(
@@ -25,24 +29,28 @@ export class WwwDotBenwainwrightDotMeStack extends cdk.Stack {
         originConfigs: [
           {
             s3OriginSource: {
-              s3BucketSource: bucket,
+              s3BucketSource: bucket
             },
-            behaviors: [{ isDefaultBehavior: true }],
-          },
+            behaviors: [{ isDefaultBehavior: true }]
+          }
         ],
+        viewerCertificate: cloudfront.ViewerCertificate.fromCloudFrontDefaultCertificate(
+          `${recordName}.${domainName}`
+        )
       }
     );
 
     const zone = new route53.HostedZone(this, "BensWebsiteBucket", {
-      zoneName: `${recordName}.${domainName}`,
+      zoneName: `${recordName}.${domainName}`
     });
 
+    zon;
     new route53.ARecord(this, "BensWebsiteBucketRecord", {
       zone,
       recordName,
       target: route53.RecordTarget.fromAlias(
         new route53Targets.CloudFrontTarget(distribution)
-      ),
+      )
     });
   }
 }
