c74e9fd62e075ad9051a5a294c9f94144687552e
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-10-15 18:06:12 +0100

    tweak list item

--- src/components/list-item.tsx
+++ src/components/list-item.tsx
@@ -2,17 +2,17 @@ import { FC } from "react"
 import styled from "@emotion/styled"
 
 const StyledLi = styled.li`
-  font-family: "Milliard";
-  line-height: 2rem;
-  font-size: 1.15rem;
-  text-align: justify;
-  @media (max-width: 800px) {
-    line-height: 1.7rem;
-  }
+	font-family: "Milliard";
+	line-height: 2rem;
+	font-size: 1.15rem;
+	text-align: left;
+	@media (max-width: 800px) {
+		line-height: 1.7rem;
+	}
 `
 
 const ListItem: FC = props => {
-  return <StyledLi>{props.children}</StyledLi>
+	return <StyledLi>{props.children}</StyledLi>
 }
 
 export default ListItem
