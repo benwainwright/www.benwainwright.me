0ffe3afcc483fcd1eda27e9f64fd59789d1b4940
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-12-16 16:32:34 +0000

    chore: remove emotion stuff

--- test-setup.ts
+++ test-setup.ts
@@ -1,8 +1,2 @@
 import "jest-extended"
 import "@testing-library/jest-dom/extend-expect"
-
-import { createSerializer, matchers } from "@emotion/jest"
-
-expect.extend(matchers)
-
-expect.addSnapshotSerializer(createSerializer())
