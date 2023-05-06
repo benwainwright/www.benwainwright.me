84b52cd5855417753cd592a512b7b1782b9bfc59
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-05-06 14:33:22 +0100

    fix: debugging

--- gatsby-node.js
+++ gatsby-node.js
@@ -54,50 +54,47 @@ exports.createSchemaCustomization = ({ actions }) => {
 }
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  const result = await graphql(
-    `
-      query {
-        allMarkdownRemark {
-          nodes {
-            id
-            htmlAst
-            frontmatter {
-              date(formatString: "MMMM DD, YYYY")
-              last_modified
-              slug
-              published
-              description
-              title
-            }
-            fields {
-              comments {
-                author
-                message
-                timestamp
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-
-  const blogPost = path.resolve(__dirname, "./src/page-templates/blog-post.tsx")
-
-  result.data.allMarkdownRemark.nodes.forEach(entry => {
-    actions.createPage({
-      path: `/blog/${entry.frontmatter.slug}`,
-      component: blogPost,
-      context: {
-        entry,
-      },
-    })
-  })
+  // const result = await graphql(
+  //   `
+  //     query {
+  //       allMarkdownRemark {
+  //         nodes {
+  //           id
+  //           htmlAst
+  //           frontmatter {
+  //             date(formatString: "MMMM DD, YYYY")
+  //             last_modified
+  //             slug
+  //             published
+  //             description
+  //             title
+  //           }
+  //           fields {
+  //             comments {
+  //               author
+  //               message
+  //               timestamp
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `
+  // )
+  // if (result.errors) {
+  //   reporter.panicOnBuild(`Error while running GraphQL query.`)
+  //   return
+  // }
+  // const blogPost = path.resolve(__dirname, "./src/page-templates/blog-post.tsx")
+  // result.data.allMarkdownRemark.nodes.forEach(entry => {
+  //   actions.createPage({
+  //     path: `/blog/${entry.frontmatter.slug}`,
+  //     component: blogPost,
+  //     context: {
+  //       entry,
+  //     },
+  //   })
+  // })
 }
 
 const { createFilePath } = require(`gatsby-source-filesystem`)
