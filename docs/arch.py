11179ba7e6ccaf9e898dade63b4cfb9ec2a3aeef
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-05-08 13:06:11 +0100

    chore: add arch diagram

--- docs/arch.py
+++ docs/arch.py
@@ -5,15 +5,19 @@ from diagrams.programming.framework import React
 from diagrams.aws.network import APIGateway
 from diagrams.aws.compute import Lambda 
 from diagrams.aws.database import Dynamodb
-from diagrams.onprem.ci import GithubActions
+from diagrams.onprem.vcs import Github
+
 
 
 
 with Diagram(show=False):
     frontend = React("Client") >> Route53("DNS")
     static_bucket = S3("Static Assets")
-    frontend >> CloudFront("CDN") - Edge(minlen="0") - static_bucket
-    frontend >> APIGateway("API") >> Lambda("Posts CRUD") >> Dynamodb("Posts") >> GithubActions("Trigger Rebuild") >> static_bucket
+    frontend >> Edge(minlen="0") >> CloudFront("CDN") >> Edge(minlen="0") >> static_bucket
+    github = Github("Trigger Rebuild")
+    backend = frontend >> APIGateway("API")
+    backend  >> Lambda("Posts CRUD") >> Dynamodb("Posts") >> github >> static_bucket
+    backend  >> Lambda("Comments CRUD") >> Dynamodb("Comments") >> github
     
 
 
